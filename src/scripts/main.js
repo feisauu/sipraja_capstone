67468f6daa4ca7329c06557f0583fb678d9373a1
Fatiha Shafiyatun N <129923674+fatihashfy@users.noreply.github.com>
2024-11-25 19:49:02 +0700

    main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,16 @@ import createLaporanPage from './pages/laporan.js';
 // eslint-disable-next-line import/extensions
 import createDetailLaporanPage from './pages/detail-laporan.js';
 
-// Fungsi routing sederhana
 const router = () => {
-  const path = window.location.hash; // Ambil hash dari URL
-  document.body.innerHTML = ''; // Bersihkan halaman
+  const path = window.location.hash; 
+  document.body.innerHTML = ''; 
 
   if (path === '#detail-laporan') {
     createDetailLaporanPage();
   } else {
-    createLaporanPage(); // Default ke halaman laporan
+    createLaporanPage();
   }
 };
 
-// Jalankan router saat pertama kali halaman dimuat
 window.addEventListener('load', router);
-// Jalankan router setiap kali hash berubah
 window.addEventListener('hashchange', router);
