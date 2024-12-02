5b9375f5c3796341f90b6ddf0b1d376d768b9a95
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-02 21:15:03 +0700

    Create endpoint.js

---
+++ src/scripts/globals/endpoint.js
@@ -0,0 +1,10 @@
+import CONFIG from './config';
+
+const ENDPOINT = {
+// POST register
+  REGISTER: `${CONFIG.REPORT}user/register`,
+  LOGIN: `${CONFIG.REPORT}user/login`,
+  
+};
+
+export default ENDPOINT;
