c6a83eff26e9ece5575be88bca44018bc6959362
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-04 22:15:13 +0700

    Update endpoint.js

--- src/scripts/globals/endpoint.js
+++ src/scripts/globals/endpoint.js
@@ -4,7 +4,8 @@ const ENDPOINT = {
 // POST register
   REGISTER: `${CONFIG.REPORT}user/register`,
   LOGIN: `${CONFIG.REPORT}user/login`,
-  
+  CREATELAPORAN: `${CONFIG.REPORT}laporan/create`,
+  GETLAPORAN: `${CONFIG.REPORT}laporan/`,
 };
 
 export default ENDPOINT;
