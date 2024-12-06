493bf524f946c636ce3e3e3d5c789ef270fd2656
053_MIB_Feis Aulia <70646439+feisauu@users.noreply.github.com>
2024-12-06 22:07:17 +0700

    mengubah button menuju login

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -4,8 +4,8 @@ class HeroSection extends HTMLElement {
       <section class="hero" id="home">
         <div class="hero-content">
           <h1>Lapor <span class="highlight">Kerusakan</span>, Jaga Kenyaman <span class="highlight">Kota Bersama</span>!</h1>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-          <button class="cta-btn">Get Started</button>
+          <p>"Bantu kami memperbaiki kota dengan melaporkan kerusakan yang ada."</p>
+          <button class="cta-btn" onclick="window.location.href='#/login'">Get Started</button>
         </div>
         <div class="hero-image">
           <img src="../images/image1.png" alt="People reporting issues">
