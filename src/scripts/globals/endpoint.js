c9e0e4713821965e32660015a2b0187dfca103a7
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-14 17:17:01 +0700

    Update endpoint.js

--- src/scripts/globals/endpoint.js
+++ src/scripts/globals/endpoint.js
@@ -6,6 +6,12 @@ const ENDPOINT = {
   LOGIN: `${CONFIG.REPORT}user/login`,
   CREATELAPORAN: `${CONFIG.REPORT}laporan/create`,
   GETLAPORAN: `${CONFIG.REPORT}laporan/`,
+  SEARCHLAPORAN: `${CONFIG.REPORT}search`,
+  GETPROFIL: `${CONFIG.REPORT}user`,
+  UPDATEPROFIL: 'https://backend-sipraja.vercel.app/api/v1/user/profile',
+  PUTLAPORAN: `${CONFIG.REPORT}laporan/`,
+  UPDATEPASSWORD: `${CONFIG.REPORT}user/password`,
+  RESET_PASSWORD:`${CONFIG.REPORT}reset/reset-password/`,
 };
 
 export default ENDPOINT;
