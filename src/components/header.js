633c533a4ae94f20a9199c7de8e6cb24caff41a4
053_MIB_Feis Aulia <70646439+feisauu@users.noreply.github.com>
2024-12-17 18:55:14 +0700

    update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,15 @@ class HeaderComponent extends HTMLElement {
     <header>
       <nav>
         <div class="navbar-left">
-              <div class="logo">
-                <span class="logo-icon">S</span>
-              <span class="logo-text">SIPRAJA</span>
-              </div>
-            </div>
+          <div class="logo">
+            <span class="logo-icon">S</span>
+            <span class="logo-text">SIPRAJA</span>
+          </div>
+        </div>
         <div class="hamburger" id="hamburger">
           <i class="fas fa-bars"></i>
         </div>
-        <div class="auth-buttons">
+        <div class="auth-buttons" id="authButtons">
           <button class="login" id="loginButton">Login</button>
           <button class="signup" id="signupButton">Sign Up</button>
         </div>
@@ -30,6 +30,13 @@ class HeaderComponent extends HTMLElement {
     const signupButton = this.querySelector('#signupButton');
     signupButton.addEventListener('click', () => {
       window.location.hash = '#/register';
+    });
+
+    // Event listener untuk hamburger button
+    const hamburgerButton = this.querySelector('#hamburger');
+    const authButtons = this.querySelector('#authButtons');
+    hamburgerButton.addEventListener('click', () => {
+      authButtons.classList.toggle('active');
     });
   }
 }
