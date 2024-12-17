d6ad3bca9123cd8d3008eec521118726cb8c166c
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-12-17 22:29:30 +0700

    Update laporanRouter.js

--- be-main/routes/laporanRouter.js
+++ be-main/routes/laporanRouter.js
@@ -1,18 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-const {getLaporan, createLaporan, deleteLaporan, getLaporanById, updateLaporan, accLaporan} = require('../controllers/reportControler')
+const {getLaporan, createLaporan, deleteLaporan, getLaporanById, updateLaporan, accLaporan, archiveLaporan, unarchiveLaporan} = require('../controllers/reportControler')
 const verifyToken = require('../middlewares/authMiddleware');
 const {uploadReportImage} = require('../middlewares/storage'); 
  
 
 router.get('/', verifyToken,  getLaporan);
 router.get('/:id', verifyToken, getLaporanById);
-router.post('/create',uploadReportImage, verifyToken, createLaporan)
+router.post('/create',uploadReportImage, verifyToken, createLaporan);
 router.delete('/delete/:id', verifyToken, deleteLaporan);
 router.put('/:id', uploadReportImage, verifyToken, updateLaporan);
 router.put('/acc/:id', verifyToken, accLaporan);
-
+router.put('/archive/:id', verifyToken, archiveLaporan); // Arsipkan laporan
+router.put('/unarchive/:id', verifyToken, unarchiveLaporan); // Batalkan arsipkan laporan
 
 
 module.exports = router
