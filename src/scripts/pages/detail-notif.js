3e22f41d1c1f61cc1035a7ff1846150b3af4ad52
monicaoktaviani <116496048+monicaca9@users.noreply.github.com>
2024-12-18 19:56:14 +0700

    Update detail-notif.js

--- src/scripts/pages/detail-notif.js
+++ src/scripts/pages/detail-notif.js
@@ -17,33 +17,21 @@ const createNotificationPage = () => {
   notifItem1.setAttribute('status', 'Laporan Selesai');
   notifItem1.setAttribute('time', '2 hari yang lalu');
   notifItem1.innerHTML = `
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab ad
-    consequuntur quos vel culpa? Rerum, impedit voluptates alias provident
-    magni officia nobis natus tempora harum facere accusamus amet nemo
-    aliquid enim explicabo sunt dicta, minus sed. Maxime porro quisquam
-    quasi!
+    ✅ Laporan Anda Telah Selesai Diproses! Selamat! Laporan Anda melalui SIPRAJA telah berhasil diproses dengan baik. Terima kasih atas kepercayaan Anda kepada layanan kami. Anda dapat segera memeriksa hasilnya melalui sistem. Jika ada pertanyaan lebih lanjut, jangan ragu untuk menghubungi kami.
   `;
 
   const notifItem2 = document.createElement('notif-item');
   notifItem2.setAttribute('status', 'Laporan Diproses');
   notifItem2.setAttribute('time', '2 hari yang lalu');
   notifItem2.innerHTML = `
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
-    accusantium nisi inventore ullam natus est nam, impedit nemo ipsam qui
-    beatae cupiditate enim ipsa illo doloribus molestiae ea cum consectetur
-    quos perspiciatis quisquam dolore libero. Ab quos ullam saepe
-    consequatur!
+    🔄 Laporan Anda Sedang Diproses! Terima kasih telah menggunakan layanan SIPRAJA. Tim kami saat ini sedang memproses laporan Anda. Proses ini mungkin memerlukan waktu, namun kami pastikan laporan Anda ditangani dengan sebaik-baiknya. Harap bersabar, dan kami akan segera memberikan pembaruan berikutnya.
   `;
 
   const notifItem3 = document.createElement('notif-item');
   notifItem3.setAttribute('status', 'Laporan Selesai');
   notifItem3.setAttribute('time', '2 hari yang lalu');
   notifItem3.innerHTML = `
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempora
-    natus explicabo. Atque omnis id ab nostrum modi itaque minima deleniti
-    quo molestiae rem alias, voluptas dicta ratione vel nulla molestias
-    quibusdam, unde veritatis enim iste praesentium fuga recusandae!
-    Incidunt!
+    ✅ Laporan Anda Telah Selesai Diproses! Selamat! Laporan Anda melalui SIPRAJA telah berhasil diproses dengan baik. Terima kasih atas kepercayaan Anda kepada layanan kami. Anda dapat segera memeriksa hasilnya melalui sistem. Jika ada pertanyaan lebih lanjut, jangan ragu untuk menghubungi kami.
   `;
 
   notifSection.appendChild(notifItem1);
