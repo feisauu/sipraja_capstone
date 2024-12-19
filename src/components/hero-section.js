9886e9d74989613a4bcd4cc57baa5948f5a8c9d7
053_MIB_Feis Aulia <70646439+feisauu@users.noreply.github.com>
2024-12-20 06:36:23 +0700

    update bahasa hero

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -3,9 +3,9 @@ class HeroSection extends HTMLElement {
     this.innerHTML = `
       <section class="hero" id="home">
         <div class="hero-content">
-          <h1>Lapor <span class="highlight">Kerusakan</span>, Jaga Kenyaman <span class="highlight">Kota Bersama</span>!</h1>
+          <h1>Lapor <span class="highlight">Kerusakan</span>, Jaga Kenyamanan <span class="highlight">Kota Bersama</span>!</h1>
           <p>"Bantu kami memperbaiki kota dengan melaporkan kerusakan yang ada."</p>
-          <button class="cta-btn" onclick="window.location.href='#/login'">Get Started</button>
+          <button class="cta-btn" onclick="window.location.href='#/login'">Mulai</button>
         </div>
         <div class="hero-image">
           <img src="../images/image1.png" alt="People reporting issues">
