1ad5603b43cb68a57901add1cfc6851bb8aca216
053_MIB_Feis Aulia <70646439+feisauu@users.noreply.github.com>
2024-12-20 07:33:32 +0700

    update interaktif hero

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-plusplus */
 class HeroSection extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <section class="hero" id="home">
         <div class="hero-content">
-          <h1>Lapor <span class="highlight">Kerusakan</span>, Jaga Kenyamanan <span class="highlight">Kota Bersama</span>!</h1>
+          <h1 id="hero-text">
+            Lapor <span class="highlight">Kerusakan</span>, Jaga Kenyamanan <span class="highlight">Kota Bersama</span>
+          </h1>
           <p>"Bantu kami memperbaiki kota dengan melaporkan kerusakan yang ada."</p>
           <button class="cta-btn" onclick="window.location.href='#/login'">Mulai</button>
         </div>
@@ -12,6 +15,49 @@ class HeroSection extends HTMLElement {
         </div>
       </section>
     `;
+    this.typeEffect();
+  }
+
+  typeEffect() {
+    const element = this.querySelector('#hero-text');
+    const text = 'Lapor <span class="highlight">Kerusakan</span>, Jaga Kenyamanan <span class="highlight">Kota Bersama!</span>';
+    element.innerHTML = '';
+    let index = 0;
+    let isTag = false;
+    let currentText = '';
+
+    function type() {
+      if (index < text.length) {
+        const currentChar = text[index];
+        if (currentChar === '<') {
+          isTag = true;
+        }
+
+        if (isTag) {
+          currentText += currentChar;
+        } else {
+          currentText += currentChar;
+        }
+
+        if (currentChar === '>') {
+          isTag = false;
+        }
+
+        element.innerHTML = `${currentText}<span class="cursor">|</span>`;
+        index++;
+        setTimeout(type, 50);
+      } else {
+        element.querySelector('.cursor').remove();
+        setTimeout(() => {
+          index = 0;
+          currentText = '';
+          element.innerHTML = '';
+          type();
+        }, 2000);
+      }
+    }
+
+    type();
   }
 }
 
